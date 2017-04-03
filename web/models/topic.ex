b95fc912c733eca5238a725abdaf4140c367bcb9
Yann VERY <yann.very@gmail.com>
2017-04-03 15:26:56 +0200

    Add topics list and creation

---
+++ web/models/topic.ex
@@ -0,0 +1,17 @@
+defmodule Discuss.Topic do
+  use Discuss.Web, :model
+
+  schema "topics" do
+    field :title, :string
+  end
+
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:title])
+    |> validate_required([:title])
+  end
+
+  def test_cast(struct, params, keys) do
+    cast(struct, params, keys)
+  end
+end
